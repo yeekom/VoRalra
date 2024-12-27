78bf52623ec04edf1f12e8b801140a0d4588931d
NotValra <124619531+NotValra@users.noreply.github.com>
2024-12-27 20:04:24 +0100

    Delete popup.js

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("Show update").addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: 'showUpdateOverlayFromPopup' }, response => {
-        if (response) {
-            console.log(response);
-        } else {
-            console.error("No response received from background script.");
-        }
-    });
-});
